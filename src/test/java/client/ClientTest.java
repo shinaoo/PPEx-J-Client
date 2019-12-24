8a2515cb7c999656a2c085e4eec880419c54e74e
shinaoo <125859320@qq.com>
2019-12-24 21:04:39 +0800

    初步测试成功

--- src/test/java/client/ClientTest.java
+++ src/test/java/client/ClientTest.java
@@ -80,7 +80,7 @@ public class ClientTest {
     @Test
     public void Rudp2Test() throws Exception {
         client.sendTestRudp2();
-        TimeUnit.SECONDS.sleep(5);
+        TimeUnit.SECONDS.sleep(2);
     }
 
 }
